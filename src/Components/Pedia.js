a204ef31859ff04e8e9fb2060ad6ca793319cb05
Nicolas Mays <ngmays1@gmail.com>
2020-12-21 11:51:19 -0500

    added animation or hunter links

--- src/Components/Pedia.js
+++ src/Components/Pedia.js
@@ -1,5 +1,6 @@
 import React, { useState }from 'react';
 import {Link} from 'react-router-dom';
+import { useSpring, animated } from 'react-spring';
 
 function Pedia() {
     const [hunters, setHunters] = useState([
@@ -18,6 +19,13 @@ function Pedia() {
             nen:300
         }
     ]);
+
+    const flyin = useSpring({
+        from: {opacity: 0, marginLeft: -100, marginRight: 100},
+        to: { opacity: 1, marginLeft: 0, marginRight: 0 } 
+    });
+
+    
 /*
     const drainAura = (index, amount=10) => {
         const newHunters = hunters;
@@ -38,22 +46,29 @@ function Pedia() {
     }
 
    */ 
+
+   const sendHunter = hunter => {
+       console.log(hunter);
+   }
     return (
         <div>
             <div>
                 <h1>HunterPedia:</h1>
-                {hunters.map((hunter, index) => (
-                    
-                    <div
-                    key={index}>
+                {hunters.map((hunter, index) => (  
+                    <animated.div
+                    key={index}
+                    style={flyin}>
                     <Link to={{
                         pathname:`/hunter/${index, hunter.name}`,
                         state:{ hunter }}}
                     >
                         <h2> { hunter.name } </h2>
                     </Link>
-                    </div>
+                    <input type='checkbox' name='hunter' value={sendHunter(hunter)}></input>
+
+                    </animated.div>
                 ))}
+                <button type='submit'></button>
             </div>
         </div>
     )
